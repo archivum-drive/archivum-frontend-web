e9b7cbbc84e6ede97e1e210a58ee2862df6786d2
Tristan Jockel <tristan.jockel@icloud.com>
2025-12-22 14:31:18 +0100

    chore: added archivum-code-wasm dep

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,14 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
+import wasm from "vite-plugin-wasm";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    wasm(),
+    topLevelAwait(),
     react(),
     tailwindcss(),
     tanstackRouter({ target: "react", autoCodeSplitting: true }),
